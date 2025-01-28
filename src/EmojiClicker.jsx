9e2ecfebc3b0285ad900f3fc89be27cdf46a1625
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-01-29 00:16:36 +0530

    add delete component to delte emoji

--- src/EmojiClicker.jsx
+++ src/EmojiClicker.jsx
@@ -6,10 +6,18 @@ export default function EmojiClicker() {
     const addEmojies = () => {
         setEmojies((oldemojies) => [...oldemojies, { id: uuid(), emoji: "🍏" }])
     }
+    const deleteEmoji = (id) => {
+        setEmojies((previousemojies) => {
+            return previousemojies.filter(e => e.id !== id)
+        })
+    }
+
     return (
         <div>
             {emojies.map((e) => (
-                <span key={e.id} style={{ fontSize: "4rem" }} >{e.emoji}</span>
+                <span
+                    onClick={() => deleteEmoji(e.id)}
+                    key={e.id} style={{ fontSize: "4rem" }} >{e.emoji}</span>
             ))}
             <button onClick={addEmojies} >Add Emojie</button>
         </div>
