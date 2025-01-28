19fba64e450ea5d1ec6df8004117223b9791416c
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-01-28 17:17:31 +0530

    Add lazy initial state setup for complex state in Dumbo component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
+import Dumbo from './Dumbo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Dumbo />
       <h1>state Demo!</h1>
-      <Counter />
+      {/* <Counter /> */}
     </>
   )
 }
